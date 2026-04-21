cd43d93d47b8cbea8ccc3e0ed1d94eb0bd13e11f
John Vining <johnvining@gmail.com>
2026-04-21 11:19:17 -0500

    add .htm rewrite middleware for dev server

--- astro-src/astro.config.mjs
+++ astro-src/astro.config.mjs
@@ -5,4 +5,17 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   build: { format: 'file' },
   markdown: { smartypants: false },
+  vite: {
+    plugins: [{
+      name: 'htm-rewrite',
+      configureServer(server) {
+        server.middlewares.use((req, _res, next) => {
+          if (req.url?.endsWith('.htm')) {
+            req.url = req.url.slice(0, -4);
+          }
+          next();
+        });
+      }
+    }]
+  }
 });
